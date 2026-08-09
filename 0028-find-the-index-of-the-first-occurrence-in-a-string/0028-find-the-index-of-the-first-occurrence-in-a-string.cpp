0c777a87ef2d4637cdb02636ad4362b9a8931d6f
Prankur <mrmanojsingh1008@gmail.com>
2026-08-09 22:36:29 +0530

    Time: 3 ms (18.55%), Space: 9.5 MB (7.51%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -2,22 +2,42 @@ class Solution {
 public:
     int strStr(string haystack, string needle) {
         int n=haystack.size(),m=needle.size();
-        for(int i=0;i<=(n-m);i++){
-            int f=i,s=0;
-            while(s<m){
-                if(haystack[f]!=needle[s]){
-                    break;
+        //lps of needle
+        int pre=0,suf=1;
+        vector<int>lps(m,0);
+        while(suf<m){
+            if(needle[pre]==needle[suf]){
+                lps[suf]=pre+1;
+                pre++,suf++;
+            }
+            else{
+                if(pre==0){
+                    lps[suf]=0;
+                    suf++;
                 }
                 else{
-                    f++;
-                    s++;
-                }
-                if(s==m){
-                    return f-s;
+                    pre=lps[pre-1];
                 }
             }
-           
         }
-         return -1;
+        //KMP Matching
+        int first=0,sec=0;
+        while(first<n && sec<m){
+            if(haystack[first]==needle[sec]){
+                first++,sec++;
+            }
+            else{
+                if(sec==0){
+                    first++;
+                }
+                else{
+                    sec=lps[sec-1];
+                }
+            }
+            if(sec==m){
+                return first-sec;
+            }
+        }
+        return -1;
     }
 };
